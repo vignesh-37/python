807a8dd77cada932fa7c6030df3207daad08b2ae
vignesh-37 <vigneshvengadesh034@gmail.com>
2024-02-08 09:41:48 +0530

    i am added a new file for add_array.py,in this flie i added first in the folder

---
+++ add_array.py
@@ -0,0 +1,20 @@
+def add_array(num):
+    arr=[]
+    if num <=15:
+        for i in range(num):
+            i=int(input())
+            arr.append(i)
+        add=0
+        even=0
+        for j in arr:
+            if j%2 == 0:
+                even +=j
+            else:
+                add +=j
+        print("The sum of the even numbers in the array is",add)
+        print("The sum of the odd numbers in the array is",even)
+    else:
+        pass
+
+a=int(input())
+add_array(a)
